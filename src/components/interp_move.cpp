433200f3a70d8ecd5f35cc751cac979b7f601d55
Casper Turesson <Casperture@gmail.com>
2026-03-13 12:40:53 +0100

    Implemented InterpMove

---
+++ src/components/interp_move.cpp
@@ -0,0 +1,158 @@
+#include "interp_move.hpp"
+#include "components/transform.hpp"
+#include "core/logging.hpp"
+#include "scene/entity.hpp"
+
+static XMFLOAT3 operator+(const XMFLOAT3 &a, const XMFLOAT3 &b) {
+    return {
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z
+    };
+}
+
+static XMFLOAT3 operator-(const XMFLOAT3 &a, const XMFLOAT3 &b) {
+    return {
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z
+    };
+}
+
+static XMFLOAT3 operator*(const XMFLOAT3 &v, float s) {
+    return {
+        v.x * s,
+        v.y * s,
+        v.z * s
+    };
+}
+
+void InterpMove::EaseHelper(float t) {
+    Transform *transform = this->GetOwner()->GetComponent<Transform>();
+    if (!transform) {
+        LogInfo("Missing Transform in InterpMove\n");
+        return;
+    }
+
+    if (!this->shouldIgnorePosition)
+        transform->SetPosition(this->startPosition + (this->endPosition - this->startPosition) * t);
+
+    if (!this->shouldIgnoreRotation)
+        transform->SetRotation(this->startRotation + (this->endRotation - this->startRotation) * t);
+
+    if (!this->shouldIgnoreScale)
+        transform->SetScale(this->startScale + (this->endScale - this->startScale) * t);
+}
+
+void InterpMove::EaseLinear() {
+    float t = this->moveTimer / this->moveDuration;
+
+    t = t;
+
+    this->EaseHelper(t);
+}
+
+void InterpMove::EaseInSine() {
+    float t = this->moveTimer / this->moveDuration;
+
+    t = 1.0f - cosf((t * XM_PI) / 2.0f);
+
+    this->EaseHelper(t);
+}
+
+void InterpMove::EaseOutSine() {
+    float t = this->moveTimer / this->moveDuration;
+
+    t = sinf((t * XM_PI) / 2.0f);
+
+    this->EaseHelper(t);
+}
+
+void InterpMove::EaseInOutSine() {
+    float t = this->moveTimer / this->moveDuration;
+
+    t = -(cosf(XM_PI * t) - 1.0f) / 2.0f;
+
+    this->EaseHelper(t);
+}
+
+void InterpMove::UpdateTimersNormal(float deltaTime) {
+    this->moveTimer += deltaTime;
+    if (this->moveTimer >= this->moveDuration) {
+        this->moveTimer = this->moveDuration;
+
+        this->pauseTimer += deltaTime;
+        if (this->pauseTimer >= this->pauseDuration) {
+            this->pauseTimer = 0.0f;
+            this->moveTimer = 0.0f;
+        }
+    }
+}
+
+void InterpMove::UpdateTimersPingPong(float deltaTime) {
+    if (this->isReverse) {
+        this->moveTimer -= deltaTime;
+        if (this->moveTimer <= 0.0f) {
+            this->moveTimer = 0.0f;
+
+            this->pauseTimer += deltaTime;
+            if (this->pauseTimer >= this->pauseDuration) {
+                this->pauseTimer = 0.0f;
+                this->isReverse = false;
+            }
+        }
+    }
+    else {
+        this->moveTimer += deltaTime;
+        if (this->moveTimer >= this->moveDuration) {
+            this->moveTimer = this->moveDuration;
+
+            this->pauseTimer += deltaTime;
+            if (this->pauseTimer >= this->pauseDuration) {
+                this->pauseTimer = 0.0f;
+                this->isReverse = true;
+            }
+        }
+    }
+}
+
+void InterpMove::OnStart(const Engine_context &context) {}
+
+void InterpMove::Update(const Frame_context &context) {
+    if (!this->isActive)
+        return;
+
+    if (this->shouldPingPong)
+        this->UpdateTimersPingPong(context.deltaTime);
+    else
+        this->UpdateTimersNormal(context.deltaTime);
+
+    switch (this->easingFunctionType) {
+        case (int)Easing_function_type::linear: {
+            this->EaseLinear();
+        } break;
+        case (int)Easing_function_type::inSine: {
+            this->EaseInSine();
+        } break;
+        case (int)Easing_function_type::outSine: {
+            this->EaseOutSine();
+        } break;
+        case (int)Easing_function_type::inOutSine: {
+            this->EaseInOutSine();
+        } break;
+    }
+}
+
+void InterpMove::Render(Renderer *renderer) {}
+void InterpMove::OnDestroy(const Engine_context &context) {}
+
+void InterpMove::SetShouldPingPong(bool shouldPingPong) {
+    this->shouldPingPong = shouldPingPong;
+
+    if (!shouldPingPong)
+        this->isReverse = false;
+}
+
+bool InterpMove::GetShouldPingPong() const {
+    return this->shouldPingPong;
+}
---
+++ src/components/interp_move.hpp
@@ -0,0 +1,85 @@
+#ifndef INTERP_MOVE_HPP
+#define INTERP_MOVE_HPP
+
+#include "scene/component.hpp"
+
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+enum class Easing_function_type {
+    linear = 0,
+    inSine,
+    outSine,
+    inOutSine
+};
+
+class InterpMove : public Component {
+    float moveTimer  = 0.0f;
+    float pauseTimer = 0.0f;
+
+    bool shouldPingPong = false;
+    bool isReverse      = false;
+
+    void EaseHelper(float t);
+    void EaseLinear();
+    void EaseInSine();
+    void EaseOutSine();
+    void EaseInOutSine();
+
+    void UpdateTimersNormal(float deltaTime);
+    void UpdateTimersPingPong(float deltaTime);
+
+public:
+    XMFLOAT3 startPosition = {0.0f, 0.0f, 0.0f};
+    XMFLOAT3 startRotation = {0.0f, 0.0f, 0.0f};
+    XMFLOAT3 startScale    = {1.0f, 1.0f, 1.0f};
+
+    XMFLOAT3 endPosition = {0.0f, 0.0f, 0.0f};
+    XMFLOAT3 endRotation = {0.0f, 0.0f, 0.0f};
+    XMFLOAT3 endScale    = {1.0f, 1.0f, 1.0f};
+
+    bool shouldIgnorePosition = false;
+    bool shouldIgnoreRotation = false;
+    bool shouldIgnoreScale    = false;
+
+    float moveDuration  = 1.0f;
+    float pauseDuration = 0.0f;
+
+    int easingFunctionType = (int)Easing_function_type::linear; // TODO: int to allow for reflection; better way?
+
+    InterpMove(Entity *owner, bool isActive) : Component(owner, isActive) {}
+    ~InterpMove() = default;
+
+    void OnStart(const Engine_context &context) override;
+    void Update(const Frame_context &context) override;
+    void Render(Renderer *renderer) override;
+    void OnDestroy(const Engine_context &context) override;
+
+    void Reflect(ComponentRegistry::Inspector *inspector) override {
+        BIND(startPosition);
+        BIND(startRotation);
+        BIND(startScale);
+
+        BIND(endPosition);
+        BIND(endRotation);
+        BIND(endScale);
+
+        BIND(shouldIgnorePosition);
+        BIND(shouldIgnoreRotation);
+        BIND(shouldIgnoreScale);
+
+        BIND(moveDuration);
+        BIND(pauseDuration);
+        BIND(shouldPingPong);
+
+        BIND(easingFunctionType); // TODO
+    }
+
+    void SetShouldPingPong(bool shouldPingPong);
+    bool GetShouldPingPong() const;
+};
+
+REGISTER_COMPONENT(InterpMove);
+
+#endif
